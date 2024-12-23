0eafbf9ab55b85a653dc3a9f29a01b2d4b183d9e
ioborin22 <ioborin22@gmail.com>
2024-12-22 19:05:15 -0500

    20241222

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -836,7 +836,7 @@
                         <label for="email">Email</label>
                         <input type="email" id="email" name="email" required
                             placeholder="Enter your email" />
-                        <button type="submit" class="submit-btn" onclick="gtag('event', 'conversion', { 'send_to': 'AW-10967820321' });">
+                        <button type="submit" class="submit-btn" onclick="gtag('event', 'conversion', { 'send_to': 'AW-10967820321/-pRNCMPv7voZEKHQ7u0o' });">
                             <span class="btn-text">Launch My Website</span>
                             <span class="spinner" style="display: none;"></span>
                         </button>
